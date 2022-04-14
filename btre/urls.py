e47383139faa8441233c1784b92e47007f5965c3
Sam <samd3ham@gmail.com>
2022-04-13 20:38:26 -0400

    add templates to pages app

--- btre/urls.py
+++ btre/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
 
-urlpatterns = [
+urlpatterns= [
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
 ]
+
+
+# An enpoint in an api is code that lives on the backend and 
+# serves clients with data when given a request 
